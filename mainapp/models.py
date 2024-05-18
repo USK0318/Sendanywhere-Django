dd636b81416f02789e8102d1c5a828daa65d8b12
usk0318 <saikiranuppalapati2@gmail.com>
2024-05-18 12:16:49 +0530

    .

--- mainapp/models.py
+++ mainapp/models.py
@@ -6,6 +6,6 @@ from django.conf import settings
 # Create your models here.
 class gamedata(models.Model):
     id=models.IntegerField(primary_key=True)
-    data=models.ImageField(upload_to='gamedata/')
+    data=models.FileField(upload_to='gamedata/')
 
   
